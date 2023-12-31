0f830cb44832f3874f1b1e83786a809fad751610
saseungmin <dbd02169@naver.com>
2023-12-31 18:37:29 +0900

    feat: init project
    - 프로젝트 세팅
    - next.js

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFiles: [],
+  setupFilesAfterEnv: [
+    '<rootDir>/jest.setup.ts',
+  ],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  testPathIgnorePatterns: [
+    'node_modules',
+    '<rootDir>.*/public',
+    '<rootDir>/.next/',
+  ],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+};
+
+module.exports = async () => ({
+  ...await createJestConfig(customJestConfig)(),
+  transformIgnorePatterns: [],
+});
