fd5bca4b3beb3043304e3714d3ec7b5b41e40b9f
Harang <dbd02169@naver.com>
2024-01-02 00:43:51 +0900

    feat: next/font 적용 (#4)
    * feat: next/font 적용
    
    - 한글인 경우, pretendard
    - 영어인 경우, sen google font
    
    * fix: ci test error

---
+++ src/app/fonts/index.ts
@@ -0,0 +1,54 @@
+import { Sen } from 'next/font/google';
+import localFont from 'next/font/local';
+
+export const pretendardFont = localFont({
+  src: [
+    {
+      path: './Pretendard-Bold.subset.woff2',
+      weight: 'bold',
+      style: 'normal',
+    },
+    {
+      path: './Pretendard-SemiBold.subset.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './Pretendard-Medium.subset.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './Pretendard-Regular.subset.woff2',
+      weight: 'normal',
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+  preload: true,
+  variable: '--font-pretendard',
+  fallback: [
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'system-ui',
+    'Roboto',
+    'Helvetica Neue',
+    'Segoe UI',
+    'Apple SD Gothic Neo',
+    'Noto Sans KR',
+    'Malgun Gothic',
+    'Apple Color Emoji',
+    'Segoe UI Emoji',
+    'Segoe UI Symbol',
+    'sans-serif',
+  ],
+});
+
+export const senFont = Sen({
+  subsets: ['latin'],
+  display: 'swap',
+  preload: true,
+  variable: '--font-sen',
+  adjustFontFallback: false,
+  fallback: [],
+});
