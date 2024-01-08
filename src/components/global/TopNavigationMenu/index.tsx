8bc00df15458675ad5d2b34b38d25967d56ce746
Harang <dbd02169@naver.com>
2024-01-08 23:49:05 +0900

    feat: dnd about페이지의 sub navigation을 구현하라 (#11)
    * feat: dnd about페이지의 sub navigation을 구현하라
    
    * refactor: 중복된 코드 리팩터링

--- src/components/global/TopNavigationMenu/index.tsx
+++ src/components/global/TopNavigationMenu/index.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useState } from 'react';
+
 import { Route } from 'next';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -11,36 +13,62 @@ import styles from './index.module.scss';
 function TopNavigationMenu() {
   const pathname = usePathname() as Route;
 
+  const [isVisibleSubNav, setIsVisibleSubNav] = useState<boolean>(false);
+
+  const isDNDPathname = ['/dnd/about', '/dnd/culture'].includes(pathname);
+
+  const isActivePathname = (targetPathname: Route) => targetPathname === pathname;
+
   return (
     <ul className={styles.topNavigationMenuWrapper}>
       <li>
-        <Link href="/" className={clsx(pathname === '/' && styles.active)}>
+        <Link href="/" className={clsx(isActivePathname('/') && styles.active)}>
           홈
         </Link>
       </li>
       <li>
-        <Link href="/projects" className={clsx(pathname === '/projects' && styles.active)}>
+        <Link href="/projects" className={clsx(isActivePathname('/projects') && styles.active)}>
           프로젝트
         </Link>
       </li>
-      <li>
-        {/* TODO - sub navigation으로 변경 */}
-        <Link href="/dnd/about" className={clsx(pathname === '/dnd/about' && styles.active)}>
+      <li
+        className={styles.aboutNavItem}
+        onMouseEnter={() => setIsVisibleSubNav(true)}
+        onMouseLeave={() => setIsVisibleSubNav(false)}
+      >
+        <Link
+          href="/dnd/about"
+          className={clsx(isDNDPathname && styles.active)}
+        >
           DND
         </Link>
+        {(isVisibleSubNav || isDNDPathname) && (
+          <ul className={styles.subNav}>
+            <li>
+              <Link href="/dnd/about" className={clsx(styles.subNavItem, isActivePathname('/dnd/about') && styles.activeSub)}>
+                소개
+              </Link>
+            </li>
+            <li>
+              <Link href="/dnd/culture" className={clsx(styles.subNavItem, isActivePathname('/dnd/culture') && styles.activeSub)}>
+                문화
+              </Link>
+            </li>
+          </ul>
+        )}
       </li>
       <li>
-        <Link href="/reviews" className={clsx(pathname === '/reviews' && styles.active)}>
+        <Link href="/reviews" className={clsx(isActivePathname('/reviews') && styles.active)}>
           후기
         </Link>
       </li>
       <li>
-        <Link href="/blog" className={clsx(pathname === '/blog' && styles.active)}>
+        <Link href="/blog" className={clsx(isActivePathname('/blog') && styles.active)}>
           블로그
         </Link>
       </li>
       <li>
-        <Link href="/jobs" className={clsx(pathname === '/jobs' && styles.active)}>
+        <Link href="/jobs" className={clsx(isActivePathname('/jobs') && styles.active)}>
           채용
         </Link>
       </li>
