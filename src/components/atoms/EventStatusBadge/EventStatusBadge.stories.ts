4b39856070e9730bb266aa98d2236233bd67e760
saseungmin <dbd02169@naver.com>
2024-01-14 21:42:13 +0900

    feat: 스토리북 세팅 및 공통 컴포넌트 적용

---
+++ src/components/atoms/EventStatusBadge/EventStatusBadge.stories.ts
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import EventStatusBadge from '.';
+
+const meta = {
+  title: 'Components/EventStatusBadge',
+  component: EventStatusBadge,
+  parameters: {
+    layout: 'centered',
+  },
+  args: {},
+  tags: ['autodocs'],
+} satisfies Meta<typeof EventStatusBadge>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Info: Story = {
+  args: {
+    type: 'info',
+    text: 'DND는 6기 진행중',
+  },
+};
+
+export const Success: Story = {
+  args: {
+    type: 'success',
+    text: 'DND는 6기 진행중',
+  },
+};
+
+export const Warn: Story = {
+  args: {
+    type: 'warn',
+    text: 'DND는 6기 진행중',
+  },
+};
+
+export const Error: Story = {
+  args: {
+    type: 'error',
+    text: 'DND는 6기 진행중',
+  },
+};
