ff6218c38768ceb377e28086b11fc0c69357ee91
Harang <dbd02169@naver.com>
2024-01-18 00:08:36 +0900

    chore: svg 사용을 위한 @svgr/webpack 세팅 (#25)

--- next.config.js
+++ next.config.js
@@ -30,6 +30,28 @@ const nextConfig = {
   experimental: {
     typedRoutes: true,
   },
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      },
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
+
 };
 
 module.exports = nextConfig;
---
+++ src/lib/assets/icons/index.ts
@@ -0,0 +1,7 @@
+import AccordionPolygon from './accordion-polygon.svg';
+import ArrowRightIcon from './arrow-right.svg';
+
+export {
+  AccordionPolygon,
+  ArrowRightIcon,
+};
