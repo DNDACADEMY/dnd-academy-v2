b987849e9677f729c38b5461f1c6289a294d6ded
saseungmin <dbd02169@naver.com>
2024-01-22 20:39:52 +0900

    feat: errorboundary 적용

--- src/hooks/queries/useGetProjectsQuery.ts
+++ src/hooks/queries/useGetProjectsQuery.ts
@@ -1,10 +1,10 @@
-import { useQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 
 import api from '@/app/api';
 import { Project as ProjectType } from '@/lib/types/project';
 
 function useGetProjectsQuery({ size }: { size?: number; } = {}) {
-  const query = useQuery({
+  const query = useSuspenseQuery({
     queryKey: ['projects'],
     queryFn: () => api<ProjectType[], { size?: number; }>({
       url: '/projects',
