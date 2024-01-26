c693e652a6a3c4409ad9960730de887057a4fcfc
Harang <dbd02169@naver.com>
2024-01-26 21:27:59 +0900

    feat: 홈 > 후원사 섹션 구현 (#33)
    * refactor: api 구조 리팩터링
    
    * feat: 홈 > 후원사 섹션 구현
    
    * fix: build error

--- src/components/atoms/SectionTitle/index.tsx
+++ src/components/atoms/SectionTitle/index.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, ReactElement, ReactNode } from 'react';
 
 import clsx from 'clsx';
 
@@ -6,13 +6,19 @@ import styles from './index.module.scss';
 
 type Props = {
   title: string;
+  subTitle?: ReactElement | ReactNode;
   fullWidth?: boolean;
 };
 
-function SectionTitle({ title, fullWidth, children }: PropsWithChildren<Props>) {
+function SectionTitle({
+  title, subTitle, fullWidth, children,
+}: PropsWithChildren<Props>) {
   return (
     <section className={clsx(styles.section, fullWidth && styles.fullWidth)}>
-      <h2 className={styles.title}>{title}</h2>
+      <div className={styles.titleWrapper}>
+        <h2 className={styles.title}>{title}</h2>
+        {subTitle && <div className={styles.subTitle}>{subTitle}</div>}
+      </div>
       {children}
     </section>
   );
--- src/lib/assets/icons/index.ts
+++ src/lib/assets/icons/index.ts
@@ -1,7 +1,9 @@
 import AccordionPolygon from './accordion-polygon.svg';
 import ArrowRightIcon from './arrow-right.svg';
+import LinkIcon from './link.svg';
 
 export {
   AccordionPolygon,
   ArrowRightIcon,
+  LinkIcon,
 };
