fd414b10fb1974fc0b8436bab2c477e9001bcbca
Harang <dbd02169@naver.com>
2024-01-30 22:42:05 +0900

    feat: global footer 구현 (#36)

---
+++ src/lib/assets/logos/index.ts
@@ -0,0 +1,13 @@
+import FacebookLogo from './facebook.svg';
+import GithubLogo from './github.svg';
+import InstagramLogo from './instagram.svg';
+import LinkedInLogo from './linkedin.svg';
+import YoutubeLogo from './youtube.svg';
+
+export {
+  FacebookLogo,
+  GithubLogo,
+  InstagramLogo,
+  LinkedInLogo,
+  YoutubeLogo,
+};
