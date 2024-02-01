b84fe679021db2680f92e3003ccfbc0ac95fdc24
saseungmin <dbd02169@naver.com>
2024-02-01 23:52:58 +0900

    feat: project api 구현 및 캐시 1시간 적용

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,13 +1,18 @@
 import PageTitle from '@/components/molecules/PageTitle';
 import ShareAlarmSection from '@/components/molecules/ShareAlarmSection';
+import ProjectsPage from '@/components/pages/ProjectsPage';
+import getProjects from '@/lib/apis/project';
 
-function ProjectsPage() {
+async function Page() {
+  const projects = await getProjects();
+
   return (
     <>
       <PageTitle title="프로젝트" subTitle="참가자들의 다양한 프로젝트를 확인해 보세요!" />
+      <ProjectsPage projects={projects} />
       <ShareAlarmSection />
     </>
   );
 }
 
-export default ProjectsPage;
+export default Page;
