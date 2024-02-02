ae52a3bfb3cd5be35175a4b1279d096cc8e27026
Harang <dbd02169@naver.com>
2024-02-03 00:55:41 +0900

    feat: project tag 컴포넌트 구현 및 프로젝트 리스트 스타일 수정 (#42)
    * feat: project tag 컴포넌트 구현 및 프로젝트 리스트 스타일 수정
    
    * refactor: 불필요한 코드 삭제

---
+++ src/components/atoms/Tag/index.tsx
@@ -0,0 +1,17 @@
+import clsx from 'clsx';
+
+import styles from './index.module.scss';
+
+type Props = {
+  title: string;
+  isActive?: boolean;
+  onClick?: () => void;
+};
+
+function Tag({ title, onClick, isActive }: Props) {
+  return (
+    <button type="button" onClick={onClick} className={clsx(styles.tag, isActive && styles.active)}>{title}</button>
+  );
+}
+
+export default Tag;
