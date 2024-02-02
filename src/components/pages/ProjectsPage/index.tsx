fe060b2a899c267be8138909fb3d76a821f3bfec
saseungmin <dbd02169@naver.com>
2024-02-02 23:58:29 +0900

    feat: 프로젝트 컴포넌트 구현 및 리스트 적용

--- src/components/pages/ProjectsPage/index.tsx
+++ src/components/pages/ProjectsPage/index.tsx
@@ -1,16 +1,31 @@
 'use client';
 
+import ProjectCard from '@/components/atoms/ProjectCard';
 import { Project } from '@/lib/types/project';
+
+import styles from './index.module.scss';
 
 type Props = {
   projects: Project[];
 };
 
 function ProjectsPage({ projects }: Props) {
-  console.log(projects);
-
   return (
-    <div>ProjectsPage</div>
+    <div className={styles.projectWrapper}>
+      {projects.map(({
+        flag, id, title, thumbnail, skill, name,
+      }) => (
+        <ProjectCard
+          key={id}
+          id={id}
+          flag={flag}
+          skills={skill}
+          thumbnail={thumbnail}
+          title={title}
+          name={name}
+        />
+      ))}
+    </div>
   );
 }
 
