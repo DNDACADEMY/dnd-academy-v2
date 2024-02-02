6b1049ef1a4eec0099f5df4e4e0a85b632556e5e
Harang <dbd02169@naver.com>
2024-02-02 23:59:41 +0900

    Feat/project component (#41)
    * feat: project api 구현 및 캐시 1시간 적용
    
    * feat: 프로젝트 컴포넌트 구현 및 리스트 적용

---
+++ src/components/atoms/ProjectCard/index.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import styles from './index.module.scss';
+
+type Props = {
+  thumbnail: string;
+  title: string;
+  name: string;
+  skills: string[];
+  flag: string;
+  id: number;
+};
+
+function ProjectCard({
+  thumbnail, title, name, skills, flag, id,
+}: Props) {
+  return (
+    <Link className={styles.projectCardWrapper} href={`/projects/${id}`}>
+      <Image src={thumbnail} alt={name} width={271} height={158} className={styles.thumbnail} />
+      <div className={styles.contents}>
+        <div className={styles.flag}>{flag}</div>
+        <div className={styles.name}>{name}</div>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.skillWrapper}>
+          {skills.map((skill) => (
+            <div key={skill} className={styles.skillItem}>{skill}</div>
+          ))}
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+export default ProjectCard;
