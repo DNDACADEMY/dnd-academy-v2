891a945afdf8fa1127bcf803c23e09bd4946fbce
Harang <dbd02169@naver.com>
2024-02-03 22:20:30 +0900

    chore: project card 컴포넌트 테스트 및 스토리북 적용 (#43)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,7 @@
+import dayjs from 'dayjs';
 import qs from 'qs';
+
+const CACHE_MINUTE = 5;
 
 export const paramsSerializer = <T>(params: T): string => qs.stringify(params, {
   arrayFormat: 'comma',
@@ -6,3 +9,14 @@ export const paramsSerializer = <T>(params: T): string => qs.stringify(params, {
 });
 
 export const isSSR = typeof window === 'undefined';
+
+export const getCacheDate = (cacheTime = CACHE_MINUTE) => {
+  const date = dayjs().format('YYYY-MM-DD-HH');
+  const currentMin = dayjs().get('minute');
+  const modMin = dayjs().get('minute') % cacheTime;
+  const minute = modMin === 0 ? currentMin : currentMin - modMin;
+
+  return {
+    date: `${date}-${minute}`,
+  };
+};
