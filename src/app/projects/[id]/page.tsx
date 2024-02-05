ebedb1a4cc028bd9eb47e7deb29024833a1b2408
Harang <dbd02169@naver.com>
2024-02-05 22:36:37 +0900

    feat: project, organizer static params page 적용 (#45)
    * feat: project, organizer static params page 적용
    
    - 빌드시 static page
    - 그 외 페이지인 경우 404
    
    * refactor: 타입 key string 처리 변경

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,3 +1,15 @@
+import { getProjects } from '@/lib/apis/project';
+
+export const dynamicParams = false;
+
+export async function generateStaticParams() {
+  const projects = await getProjects();
+
+  return projects.map(({ id }) => ({
+    id: `${id}`,
+  }));
+}
+
 function ProjectPage({ params }: { params: { id: number; } }) {
   return (
     <div>{`Project ${params.id}`}</div>
