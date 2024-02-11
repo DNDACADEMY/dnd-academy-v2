115ea9877b6efb9d812f98db1f1e64197a5bb9b8
saseungmin <dbd02169@naver.com>
2024-02-11 18:47:18 +0900

    feat: review 컴포넌트 구현

--- src/components/molecules/ReviewList/index.tsx
+++ src/components/molecules/ReviewList/index.tsx
@@ -4,6 +4,12 @@ import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 
 import Link from 'next/link';
 
+import ExternalLink from '@/components/atoms/ExternalLink';
+import { ArrowRightIcon } from '@/lib/assets/icons';
+import {
+  EmailLogo, GithubLogo, HomeLogo, InstagramLogo, LinkedInLogo, MediumLogo, VelogLogo,
+} from '@/lib/assets/logos';
+import { Link as ReviewLink } from '@/lib/types/common';
 import { Review } from '@/lib/types/review';
 
 import styles from './index.module.scss';
@@ -13,6 +19,20 @@ type Props = {
 };
 
 function ReviewList({ reviews }: Props) {
+  const reviewLinkIcon = (key: ReviewLink) => {
+    const IconLogo = {
+      link: HomeLogo,
+      email: EmailLogo,
+      github: GithubLogo,
+      instagram: InstagramLogo,
+      linkedin: LinkedInLogo,
+      medium: MediumLogo,
+      velog: VelogLogo,
+    }[key] || HomeLogo;
+
+    return <IconLogo className={styles.socialIcon} />;
+  };
+
   return (
     <ResponsiveMasonry
       columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
@@ -22,16 +42,28 @@ function ReviewList({ reviews }: Props) {
         className={styles.reviewsWrapper}
       >
         {reviews.map(({
-          id, name, position, review, projectId,
+          id, name, position, review, projectId, links,
         }) => (
           <div key={id} className={styles.reviewCardWrapper}>
-            <div>
-              <h3>{name}</h3>
-              <div>{position}</div>
+            <div className={styles.titleWrapper}>
+              <h3 className={styles.name}>{name}</h3>
+              <div className={styles.position}>{position}</div>
             </div>
-            <div>{review}</div>
-            {projectId && (
-            <Link href={`/projects/${projectId}`}>프로젝트 보기</Link>
+            <div className={styles.review}>{review}</div>
+            {Object.values(links).some((link) => !!link) && (
+              <div className={styles.socialIconWrapper}>
+                {Object.entries(links).filter(([, link]) => !!link).map(([key, link]) => (
+                  <ExternalLink key={key} href={link}>
+                    {reviewLinkIcon(key as ReviewLink)}
+                  </ExternalLink>
+                ))}
+              </div>
+            )}
+            {typeof projectId === 'number' && (
+              <Link href={`/projects/${projectId}`} className={styles.link}>
+                <span>프로젝트 보기</span>
+                <ArrowRightIcon className={styles.icon} />
+              </Link>
             )}
           </div>
         ))}
