0a4a2373da864b05496cb2013d79ce993f018f23
Harang <dbd02169@naver.com>
2024-02-11 18:51:09 +0900

    feat: review 컴포넌트 구현 (#49)

--- src/lib/assets/logos/index.ts
+++ src/lib/assets/logos/index.ts
@@ -1,13 +1,23 @@
+import BehanceLogo from './behance.svg';
+import EmailLogo from './email.svg';
 import FacebookLogo from './facebook.svg';
 import GithubLogo from './github.svg';
+import HomeLogo from './home.svg';
 import InstagramLogo from './instagram.svg';
 import LinkedInLogo from './linkedin.svg';
+import MediumLogo from './medium.svg';
+import VelogLogo from './velog.svg';
 import YoutubeLogo from './youtube.svg';
 
 export {
+  BehanceLogo,
+  EmailLogo,
   FacebookLogo,
   GithubLogo,
+  HomeLogo,
   InstagramLogo,
   LinkedInLogo,
+  MediumLogo,
+  VelogLogo,
   YoutubeLogo,
 };
