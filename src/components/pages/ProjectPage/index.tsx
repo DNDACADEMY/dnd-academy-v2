9cab769857be35ef458be6cdda686a0155e64048
Harang <dbd02169@naver.com>
2024-02-23 21:57:51 +0900

    feat: 프로젝트 페이지의 참가자, 프로젝트 더보기 섹션 구현 (#55)

--- src/components/pages/ProjectPage/index.tsx
+++ src/components/pages/ProjectPage/index.tsx
@@ -2,6 +2,10 @@ import Link from 'next/link';
 
 import SkillTag from '@/components/atoms/SkillTag';
 import PDFViewer from '@/components/molecules/PDFViewer';
+import ProjectCards from '@/components/molecules/ProjectCards';
+import ReviewList from '@/components/molecules/ReviewList';
+import { getProjects } from '@/lib/apis/project';
+import { getReviews } from '@/lib/apis/review';
 import { RightIcon } from '@/lib/assets/icons';
 import { Project } from '@/lib/types/project';
 
@@ -11,8 +15,9 @@ type Props = {
   project: Project;
 };
 
-function ProjectPage({ project }: Props) {
-  console.log(project);
+async function ProjectPage({ project }: Props) {
+  const reviews = await getReviews({ flag: project.flag, projectId: project.id });
+  const projects = await getProjects({ ordinal: project.flag });
 
   return (
     <>
@@ -23,25 +28,37 @@ function ProjectPage({ project }: Props) {
         <RightIcon />
         <div className={styles.projectName}>{project.name}</div>
       </nav>
-      <div className={styles.projectDetail}>
-        <div>
-          <PDFViewer url={project?.pdf} />
-        </div>
-        <div className={styles.projectIntroduceTitleWrapper}>
+      <div className={styles.projectPageContents}>
+        <section className={styles.projectDetail}>
           <div>
-            <div className={styles.flag}>{project.flag}</div>
+            <PDFViewer url={project?.pdf} />
           </div>
-          <h1 className={styles.name}>{project.name}</h1>
-          <h2 className={styles.title}>{project.title}</h2>
-          <div className={styles.skillWrapper}>
-            {project.skill.map((skill) => (
-              <SkillTag key={skill} skill={skill} color="grey" />
-            ))}
+          <div className={styles.projectIntroduceTitleWrapper}>
+            <div>
+              <div className={styles.flag}>{project.flag}</div>
+            </div>
+            <h1 className={styles.name}>{project.name}</h1>
+            <h2 className={styles.title}>{project.title}</h2>
+            <div className={styles.skillWrapper}>
+              {project.skill.map((skill) => (
+                <SkillTag key={skill} skill={skill} color="grey" />
+              ))}
+            </div>
           </div>
-        </div>
-        <div>
-          <div className={styles.description}>{project.desc}</div>
-        </div>
+          <div>
+            <div className={styles.description}>{project.desc}</div>
+          </div>
+        </section>
+        <section className={styles.projectSection}>
+          <h2>참가자</h2>
+          <ReviewList reviews={reviews} hasProjectLink={false} />
+        </section>
+        <section className={styles.projectSection}>
+          <h2>프로젝트 더보기</h2>
+          <ProjectCards
+            projects={projects.filter(({ id }) => id !== project.id)}
+          />
+        </section>
       </div>
     </>
   );
