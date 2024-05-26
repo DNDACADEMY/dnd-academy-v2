38cb1d87f58010e2c1d282f33126d08b8c702559
saseungmin <dbd02169@naver.com>
2024-05-26 23:40:11 +0900

    feat: share modal 마크업 작업

--- src/components/organisms/ShareModal/index.tsx
+++ src/components/organisms/ShareModal/index.tsx
@@ -1,5 +1,9 @@
 import Button from '@/components/atoms/Button';
 import Modal from '@/components/molecules/Modal';
+import { ShareIcon } from '@/lib/assets/icons';
+import { ShareInstagramLogo, ShareKakaoLogo, ShareXLogo } from '@/lib/assets/logos';
+
+import styles from './index.module.scss';
 
 function ShareModal() {
   return (
@@ -7,13 +11,30 @@ function ShareModal() {
       <Modal.OpenButton>
         <Button type="button" buttonType="secondary" size="xLarge">친구에게 공유하기</Button>
       </Modal.OpenButton>
-      <Modal.ContentsBase title="친구에게 공유하기">
-        <div>
-          <div>
-            <Button type="button" size="large">카카오톡</Button>
-            <Button type="button" size="large">링크 복사</Button>
+      <Modal.ContentsBase title="친구에게 공유하기" size="small">
+        <div className={styles.shareModalContentsWrapper}>
+          <div className={styles.shareButtonWrapper}>
+            <button type="button" className={styles.shareButton}>
+              <ShareKakaoLogo className={styles.socialIcon} />
+              <div>카카오톡</div>
+            </button>
+            <button type="button" className={styles.shareButton}>
+              <ShareInstagramLogo className={styles.socialIcon} />
+              <div>인스타그램</div>
+            </button>
+            <button type="button" className={styles.shareButton}>
+              <ShareXLogo className={styles.socialIcon} />
+              <div>X</div>
+            </button>
           </div>
-          <Button type="button">URL 공유하기</Button>
+          <Button
+            type="button"
+            buttonType="secondary"
+            prefixIcon={<ShareIcon />}
+            className={styles.shareUrlButton}
+          >
+            URL 공유하기
+          </Button>
         </div>
       </Modal.ContentsBase>
     </Modal>
