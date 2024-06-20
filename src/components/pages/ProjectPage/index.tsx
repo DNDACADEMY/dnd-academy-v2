599520d312f2763acd26b1c00abf1af2c18b5352
saseungmin <dbd02169@naver.com>
2024-06-20 20:48:53 +0900

    style: 프로젝트 상세 페이지 반응형 적용

--- src/components/pages/ProjectPage/index.tsx
+++ src/components/pages/ProjectPage/index.tsx
@@ -40,21 +40,21 @@ function ProjectPage({ project }: Props) {
       />
       <div className={styles.projectPageContents}>
         <section className={styles.projectDetail}>
-          <div>
-            {project?.pdf ? (
+          {project?.pdf ? (
+            <div className={styles.pdfWrapper}>
               <PDFViewer url={project.pdf} />
-            ) : (
-              <div className={styles.thumbnailWrapper}>
-                <Image
-                  fill
-                  src={project.thumbnail}
-                  alt={project.name}
-                  className={styles.thumbnail}
-                  sizes="(max-width: 768px) 100vw, 768px"
-                />
-              </div>
-            )}
-          </div>
+            </div>
+          ) : (
+            <div className={styles.thumbnailWrapper}>
+              <Image
+                fill
+                src={project.thumbnail}
+                alt={project.name}
+                className={styles.thumbnail}
+                sizes="(max-width: 768px) 100vw, 768px"
+              />
+            </div>
+          )}
           <div className={styles.projectIntroduceTitleWrapper}>
             <div className={styles.header}>
               <Badge
@@ -111,11 +111,11 @@ function ProjectPage({ project }: Props) {
             <div className={styles.description}>{project.desc}</div>
           </div>
         </section>
-        <section className={styles.projectSection}>
+        <section className={styles.projectSubSection}>
           <h2>참가자</h2>
           <ReviewList reviews={reviews} hasProjectLink={false} />
         </section>
-        <section className={styles.projectSection}>
+        <section className={styles.projectSubSection}>
           <h2>프로젝트 더보기</h2>
           <ProjectCards
             projects={projects.filter(({ id }) => id !== project.id)}
