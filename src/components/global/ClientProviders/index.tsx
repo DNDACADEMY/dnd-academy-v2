e774217f0aaf150d48a3be22dc601700593e7ec3
Harang <dbd02169@naver.com>
2024-06-24 05:35:44 +0900

    feat: channel.io 기능 추가 (#111)

--- src/components/global/ClientProviders/index.tsx
+++ src/components/global/ClientProviders/index.tsx
@@ -3,6 +3,11 @@
 import { ReactNode } from 'react';
 import PerfectScrollbar from 'react-perfect-scrollbar';
 
+import * as ChannelService from '@channel.io/channel-web-sdk-loader';
+
+ChannelService.loadScript();
+ChannelService.boot({ pluginKey: process.env.NEXT_PUBLIC_CHANNEL_IO_PLUGIN_KEY });
+
 function ClientProviders({ children }: { children: ReactNode }) {
   return (
     <PerfectScrollbar>
