d7287fdd793aae83813ebde059aa1feaf11a90d4
saseungmin <dbd02169@naver.com>
2024-06-25 03:03:15 +0900

    refactor: 디자인 토큰 정의되어있는대로 폰트 적용 및 모바일에서 깨지는 스타일 수정

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,5 +1,11 @@
 export const CURRENT_FLAG = 11;
 
+export const NEXT_FLAG = CURRENT_FLAG + 1;
+
 export const DEVELOPER_APPLICATION_LINK = 'https://forms.gle/mmwaEmyksJjf12557';
 
 export const DESIGNER_APPLICATION_LINK = 'https://forms.gle/ZCUPBJiuctpibpdy9';
+
+export const NEXT_COHORT_NOTIFICATION_URL = 'https://forms.gle/nA3MLWbUyAoikWw16';
+
+export const APPLICATION_DUE_DATE_TIME = '2024/06/19 23:59:59';
