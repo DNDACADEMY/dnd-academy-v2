bb48cdccaa72ca2f1a40c1c3bf3c7d87209a873c
saseungmin <dbd02169@naver.com>
2024-07-02 21:04:35 +0900

    feat: 모노레포 세팅

---
+++ apps/web/.eslintrc.js
@@ -0,0 +1,113 @@
+module.exports = {
+  extends: ['../../.eslintrc.js'],
+  root: true,
+  env: {
+    es6: true,
+    browser: true,
+    jest: true,
+  },
+  ignorePatterns: [
+    'public/',
+    '/.next',
+    'storybook-static',
+  ],
+  extends: [
+    'airbnb',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:@next/next/recommended',
+  ],
+  plugins: [
+    'simple-import-sort',
+    'unused-imports',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['@', './src']],
+        extensions: ['.ts', '.js', '.tsx', '.json'],
+      },
+    },
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'airbnb-typescript',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      plugins: [
+        '@typescript-eslint',
+      ],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'warn',
+      },
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+    },
+    {
+      files: ['src/hooks/**/**/*.test.ts?(x)'],
+      rules: {
+        'react-hooks/rules-of-hooks': 'off',
+      },
+    },
+    {
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react', 'plugin:jest/recommended'],
+      rules: {
+      },
+    },
+    {
+      extends: ['plugin:cypress/recommended'],
+      files: ['cypress/**/*.ts'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: ['./cypress/tsconfig.json'],
+      },
+    },
+    {
+      extends: ['plugin:storybook/recommended'],
+      files: ['**/*.stories.ts?(x)'],
+      rules: {},
+    },
+  ],
+  rules: {
+    'react/require-default-props': 'off',
+    'import/order': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'simple-import-sort/imports': ['error', {
+      groups: [
+        ['^\\u0000'],
+        ['^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)'],
+        ['^react'],
+        ['^next'],
+        ['^@?\\w'],
+        ['^(@|lib|components|utils|hooks)(/.*|$)'],
+        ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+        ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+        ['^.+\\.svg$'],
+        ['^.+\\.s?css$'],
+      ],
+    }],
+    'simple-import-sort/exports': 'error',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': ['warn', {
+      vars: 'all',
+      varsIgnorePattern: '^_',
+      args: 'after-used',
+      argsIgnorePattern: '^_',
+    }],
+  },
+};
