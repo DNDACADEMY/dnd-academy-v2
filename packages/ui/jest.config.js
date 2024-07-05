d068aac5cdb20560c55f3732cbef791909612221
saseungmin <dbd02169@naver.com>
2024-07-05 17:41:41 +0900

    feat: packages에  @dnd-academy/ui 세팅

---
+++ packages/ui/jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  transform: {
+    '^.+\\.(t|j)sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          transform: {
+            react: {
+              runtime: 'automatic',
+            },
+          },
+        },
+      },
+    ],
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['node_modules'],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+    '^.+\\.(svg)$': '<rootDir>/__mocks__/svg.tsx',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+};
