740f2f3b69113fbdb3b176993db3e4c4c0c59e60
Harang <dbd02169@naver.com>
2024-07-11 13:16:52 +0900

    chore: migration storybook v8.2 (#137)

--- apps/web/.storybook/preview.tsx
+++ apps/web/.storybook/preview.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 
 import type { Preview } from "@storybook/react";
 
+import '@dnd-academy/ui/style.css';
 import 'src/styles/global.scss';
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on.*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
     },
+    tags: ["autodocs"],
     backgrounds: {
       default: 'dnd',
       values: [
