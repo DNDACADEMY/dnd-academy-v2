4e680917d343095d3b72312da3f48d803c5a4a96
saseungmin <dbd02169@naver.com>
2024-07-15 16:27:25 +0900

    chore: jest 세팅 공통화 및 @testing-library/react v16 버전 업데이트

--- apps/web/jest.config.js
+++ apps/web/jest.config.js
@@ -1,27 +1,17 @@
 const nextJest = require('next/jest');
+const baseConfig = require('../../jest.config');
 
 const createJestConfig = nextJest({
   dir: './',
 });
 
 const customJestConfig = {
-  setupFiles: [],
-  setupFilesAfterEnv: [
-    '<rootDir>/jest.setup.ts',
-  ],
-  moduleNameMapper: {
-    '@/(.*)': '<rootDir>/src/$1',
-    '\\.(css|less|scss)$': '<rootDir>/__mocks__/styleMock.js',
-    '^.+\\.(svg)$': '<rootDir>/__mocks__/svg.tsx',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
-  },
+  ...baseConfig,
   testPathIgnorePatterns: [
-    'node_modules',
+    ...baseConfig.testPathIgnorePatterns,
     '<rootDir>.*/public',
     '<rootDir>/.next/',
   ],
-  moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: 'jest-environment-jsdom',
 };
 
 module.exports = async () => ({
--- packages/ui/jest.config.js
+++ packages/ui/jest.config.js
@@ -1,3 +1,5 @@
+const baseConfig = require('../../jest.config');
+
 module.exports = {
   transform: {
     '^.+\\.(t|j)sx?$': [
@@ -13,14 +15,5 @@ module.exports = {
       },
     ],
   },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testPathIgnorePatterns: ['node_modules'],
-  moduleNameMapper: {
-    '@/(.*)': '<rootDir>/src/$1',
-    '\\.(css|less|scss)$': '<rootDir>/__mocks__/styleMock.js',
-    '^.+\\.(svg)$': '<rootDir>/__mocks__/svg.tsx',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
-  },
-  moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: 'jest-environment-jsdom',
+  ...baseConfig,
 };
