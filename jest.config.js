91444233aef91a474e39c310b116642d89b1713c
Harang <dbd02169@naver.com>
2024-07-15 16:33:11 +0900

    chore: jest 세팅 공통화 및 @testing-library/react v16 버전 업데이트 (#141)

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  setupFilesAfterEnv: ['<rootDir>/../../jest.setup.ts'],
+  testPathIgnorePatterns: ['node_modules'],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.(css|less|scss)$': '<rootDir>/../../__mocks__/styleMock.js',
+    '^.+\\.(svg)$': '<rootDir>/../../__mocks__/svg.tsx',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/../../__mocks__/fileMock.js',
+  },
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jsdom',
+};
