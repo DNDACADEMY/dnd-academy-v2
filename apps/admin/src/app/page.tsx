7bd8a84be846eaf7c1a01fc0d681ef167bb0b039
Harang <dbd02169@naver.com>
2024-07-21 00:50:27 +0900

    chore: admin에 @vercel/blob install 및 api 호출 테스트 (#149)

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -10,6 +10,15 @@ async function MainPage() {
     redirect('/login');
   }
 
+  // TODO - 제거 예정 (임시 호출 테스트)
+  const response = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_BLOB_HOST}/current_applicant_count.json`, {
+    method: 'GET',
+  });
+
+  const currentApplicantCountData = await response.json();
+
+  console.log('currentApplicantCountData', currentApplicantCountData);
+
   return (
     <>
       <h1>DND - AdminPage</h1>
