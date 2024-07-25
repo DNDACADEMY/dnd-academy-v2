736f55e35adaad6bb00db325088ac656763c36e8
Harang <dbd02169@naver.com>
2024-07-25 21:43:31 +0900

    perf: turborepo 최적화 (#155)

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
     '\\.(css|less|scss)$': 'identity-obj-proxy',
-    '^.+\\.svg(\\?react)?$': '<rootDir>/../../__mocks__/svg.tsx',
+    '^.+\\.svg(\\?react)?$': '<rootDir>/__mocks__/svg.tsx',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/../../__mocks__/fileMock.js',
   },
   moduleDirectories: ['node_modules', '<rootDir>/'],
