fbb29cabd91dac7f42eb1f9fbbb754996359b967
Harang <dbd02169@naver.com>
2024-07-29 21:04:50 +0900

    feat(admin): total count status 수정 액션 구현 (#160)

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -1,7 +1,9 @@
 import { redirect } from 'next/navigation';
 
-import { api, CurrentApplicantCount } from '@dnd-academy/core';
+import { api, type TotalCountStatus } from '@dnd-academy/core';
+import { Button, CounterCard } from '@dnd-academy/ui';
 
+import { totalCountStatusAction } from '@/actions/count';
 import { auth } from '@/auth';
 import SignOut from '@/components/auth/SignOut';
 
@@ -12,19 +14,34 @@ async function MainPage() {
     redirect('/login');
   }
 
-  const currentApplicantCountData = await api<CurrentApplicantCount>({
-    url: '/current_applicant_count.json',
+  const {
+    cumulativeApplicants, dropouts, totalParticipants, totalProjects,
+  } = await api<TotalCountStatus>({
+    url: '/total_count_status.json',
     method: 'GET',
   });
-
-  console.log('currentApplicantCountData', currentApplicantCountData);
 
   return (
     <>
       <h1>DND - AdminPage</h1>
-      <div>{currentApplicantCountData.designer}</div>
-      <div>{currentApplicantCountData.developer}</div>
       <SignOut />
+
+      <h2>지원자 수</h2>
+      <div>
+        <div>
+          <CounterCard count={cumulativeApplicants} title="누적 지원자 수" />
+          <CounterCard count={totalParticipants} title="총 참가자 수" />
+          <CounterCard count={totalProjects} title="총 프로젝트 수" suffix="개" />
+          <CounterCard count={dropouts} title="이탈자 수" color="primary" />
+        </div>
+        <form action={totalCountStatusAction}>
+          <input type="number" name="cumulativeApplicants" min={0} placeholder="누적 지원자 수" defaultValue={cumulativeApplicants} required />
+          <input type="number" name="totalParticipants" min={0} placeholder="총 참가자 수" defaultValue={totalParticipants} required />
+          <input type="number" name="totalProjects" min={0} placeholder="총 프로젝트 수" defaultValue={totalProjects} required />
+          <input type="number" name="dropouts" min={0} placeholder="이탈자 수" defaultValue={dropouts} required />
+          <Button type="submit">Submit</Button>
+        </form>
+      </div>
     </>
   );
 }
--- packages/core/src/@types/count.ts
+++ packages/core/src/@types/count.ts
@@ -2,3 +2,10 @@ export interface CurrentApplicantCount {
   developer: number;
   designer: number;
 }
+
+export interface TotalCountStatus {
+  cumulativeApplicants: number;
+  totalParticipants: number;
+  totalProjects: number;
+  dropouts: number;
+}
