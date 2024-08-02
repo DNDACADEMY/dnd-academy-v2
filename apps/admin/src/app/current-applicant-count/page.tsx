dbcd9afd37d6f2b5bf4caf4c20c59157fad3a090
saseungmin <dbd02169@naver.com>
2024-08-03 01:15:11 +0900

    feat(admin): current-applicant-count form 페이지 적용

---
+++ apps/admin/src/app/current-applicant-count/page.tsx
@@ -0,0 +1,34 @@
+import { api, type CurrentApplicantCount } from '@dnd-academy/core';
+import { Counter, PageTitle } from '@dnd-academy/ui';
+
+import CurrentApplicantCountForm from '@/components/CurrentApplicantCountForm';
+
+import styles from './page.module.scss';
+
+async function page() {
+  const currentApplicantCountData = await api<CurrentApplicantCount>({
+    url: '/current_applicant_count.json',
+    method: 'GET',
+  });
+
+  const currentApplicantCount = currentApplicantCountData.designer
+    + currentApplicantCountData.developer;
+
+  return (
+    <>
+      <PageTitle
+        title="현재 지원자 수"
+        subTitle="캐시 적용으로 실제 적용까지는 최대 5분정도 소요됩니다."
+      />
+      <div className={styles.counter}>
+        오늘까지&nbsp;
+        <Counter count={currentApplicantCount} />
+        명이 지원했어요!
+      </div>
+      <CurrentApplicantCountForm initialApplicantCount={currentApplicantCountData} />
+    </>
+
+  );
+}
+
+export default page;
--- apps/admin/src/components/Navigator/index.tsx
+++ apps/admin/src/components/Navigator/index.tsx
@@ -3,7 +3,8 @@ function Navigator() {
     <nav>
       <ul>
         <li>
-          <a href="/total-count-status">지원자 수 카드 섹션 정보 업데이트하기</a>
+          <a href="/current-applicant-count">현재 지원자 수 카운트 업데이트하기</a>
+          <a href="/total-count-status">총 지원자 수 카드 섹션 정보 업데이트하기</a>
         </li>
       </ul>
     </nav>
