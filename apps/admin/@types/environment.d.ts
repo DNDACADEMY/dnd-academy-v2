c14a8e9257122844aadb1d9bfa9e1c7affe163e8
saseungmin <dbd02169@naver.com>
2024-08-20 20:13:50 +0900

    feat(admin): 지원자 수 google spreadsheet 연동 후 cron 적용하여 1시간마다 데이터 가져오기 구현

--- apps/admin/@types/environment.d.ts
+++ apps/admin/@types/environment.d.ts
@@ -1,10 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 namespace NodeJS {
   interface ProcessEnv extends NodeJS.ProcessEnv {
+    AUTH_SECRET: string;
     AUTH_GOOGLE_ID: string;
     AUTH_GOOGLE_SECRET: string;
     ALLOWED_EMAIL_ADDRESSES: string;
     DND_ACADEMY_V2_BLOB_READ_WRITE_TOKEN: string;
-    AUTH_SECRET: string;
+    GOOGLE_CLIENT_EMAIL: string;
+    GOOGLE_PRIVATE_KEY: string;
+    CRON_SECRET: string;
   }
 }
