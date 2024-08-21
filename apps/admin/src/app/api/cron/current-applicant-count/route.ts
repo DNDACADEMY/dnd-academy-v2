ef29010693b1682f86822d3cc0d431186ecb377c
saseungmin <dbd02169@naver.com>
2024-08-21 22:38:03 +0900

    fix(admin): cron 유료로 클릭시 최신반영 플로우로 변경

--- apps/admin/src/app/api/cron/current-applicant-count/route.ts
+++ apps/admin/src/app/api/cron/current-applicant-count/route.ts
@@ -1,51 +1,21 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-import { put } from '@vercel/blob';
-import { JWT } from 'google-auth-library';
-import { GoogleSpreadsheet } from 'google-spreadsheet';
+import { updateCurrentApplicantCount } from '@/app/api/handler';
 
 export const dynamic = 'force-dynamic';
 
 export const runtime = 'nodejs';
 
-export async function GET(request: NextRequest) {
+export async function PUT(request: NextRequest) {
   const authHeader = request.headers.get('authorization');
+
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
     return new Response('Unauthorized', {
       status: 401,
     });
   }
 
-  const serviceAccountAuth = new JWT({
-    email: process.env.GOOGLE_CLIENT_EMAIL,
-    key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-  });
-
-  const developerApplicantDoc = new GoogleSpreadsheet('1LLxVCTkqtTZoMftrEkYOlwVdyEwoEKwCg7WvXwfW2Rk', serviceAccountAuth);
-  const designerApplicantDoc = new GoogleSpreadsheet('1BToiD3gjzT-SKWeKnQMRuFvGXmYv44oEigSIJy_w1Jc', serviceAccountAuth);
-
-  await developerApplicantDoc.loadInfo();
-  await designerApplicantDoc.loadInfo();
-
-  const developerApplicantRows = await developerApplicantDoc.sheetsByIndex[0].getRows();
-  const designerApplicantRows = await designerApplicantDoc.sheetsByIndex[0].getRows();
-
-  const currentApplicantCountForm = {
-    developer: developerApplicantRows.length,
-    designer: designerApplicantRows.length,
-  };
-
-  const jsonString = JSON.stringify(currentApplicantCountForm);
-
-  const requestBlob = new Blob([jsonString], { type: 'application/json' });
-
-  await put('current_applicant_count.json', requestBlob, {
-    access: 'public',
-    token: process.env.DND_ACADEMY_V2_BLOB_READ_WRITE_TOKEN,
-    addRandomSuffix: false,
-    cacheControlMaxAge: 3600,
-  });
+  const currentApplicantCountForm = await updateCurrentApplicantCount();
 
   return NextResponse.json(currentApplicantCountForm, {
     status: 200,
