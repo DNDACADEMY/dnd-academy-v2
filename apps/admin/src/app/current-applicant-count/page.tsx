8e278c2b4ccaecac0acd7f457b61b18d75614a91
saseungmin <dbd02169@naver.com>
2024-09-09 18:57:41 +0900

    fix: build error

--- apps/admin/src/app/current-applicant-count/page.tsx
+++ apps/admin/src/app/current-applicant-count/page.tsx
@@ -1,6 +1,7 @@
-import { api, CurrentApplicantCount } from '@dnd-academy/core';
+import { CurrentApplicantCount } from '@dnd-academy/core';
 import { Counter, PageTitle } from '@dnd-academy/ui';
 
+import ServerErrorBoundary from '@/components/common/ServerErrorBoundary';
 import CurrentApplicantCountAction from '@/components/CurrentApplicantCountAction';
 
 import styles from './page.module.scss';
@@ -8,29 +9,29 @@ import styles from './page.module.scss';
 async function page() {
   const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
 
-  const currentApplicantCountData = await api<CurrentApplicantCount>({
-    url: `${protocol}://${process.env.VERCEL_URL}/api/blob/latest/current_applicant_count`,
-  });
-
-  const currentApplicantCount = currentApplicantCountData.designer
-    + currentApplicantCountData.developer;
-
   return (
-    <>
-      <PageTitle
-        title="현재 지원자 수"
-        subTitle="현재 지원자 수는 google form의 spreadsheet를 통해 실시간으로 업데이트 됩니다."
-      />
-      <div className={styles.counter}>
-        오늘까지&nbsp;
-        <Counter count={currentApplicantCount} />
-        명이 지원했어요!
-      </div>
-      <CurrentApplicantCountAction />
-      <strong>
-        기본적으로 매일 00:00에 자동으로 반영되지만, 클릭시에는 즉시 반영할 수 있습니다.
-      </strong>
-    </>
+    <ServerErrorBoundary<CurrentApplicantCount> apiRequest={{
+      url: `${protocol}://${process.env.VERCEL_URL}/api/blob/latest/current_applicant_count`,
+    }}
+    >
+      {({ designer, developer }) => (
+        <>
+          <PageTitle
+            title="현재 지원자 수"
+            subTitle="현재 지원자 수는 google form의 spreadsheet를 통해 실시간으로 업데이트 됩니다."
+          />
+          <div className={styles.counter}>
+            오늘까지&nbsp;
+            <Counter count={designer + developer} />
+            명이 지원했어요!
+          </div>
+          <CurrentApplicantCountAction />
+          <strong>
+            기본적으로 매일 00:00에 자동으로 반영되지만, 클릭시에는 즉시 반영할 수 있습니다.
+          </strong>
+        </>
+      )}
+    </ServerErrorBoundary>
   );
 }
 
