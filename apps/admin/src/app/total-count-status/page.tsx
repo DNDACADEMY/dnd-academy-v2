637f72794e9823e587ebdaa2418e25c773773640
Harang <dbd02169@naver.com>
2024-09-12 22:43:41 +0900

    Revert "chore(admin): server component에 header 사용 제거 (#188)"
    This reverts commit dba8cadcdd0c3e3d5929564b6f43a7f400d1f954.

--- apps/admin/src/app/total-count-status/page.tsx
+++ apps/admin/src/app/total-count-status/page.tsx
@@ -1,40 +1,41 @@
-import { type TotalCountStatus } from '@dnd-academy/core';
+import { headers } from 'next/headers';
+
+import { api, type TotalCountStatus } from '@dnd-academy/core';
 import { CounterCard, PageTitle } from '@dnd-academy/ui';
 
-import ServerErrorBoundary from '@/components/common/ServerErrorBoundary';
 import TotalCountStatusForm from '@/components/TotalCountStatusForm';
 
 import styles from './page.module.scss';
 
 async function page() {
+  const headersList = headers();
+
+  const host = headersList.get('host');
+
   const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
 
+  const totalCountStatus = await api<TotalCountStatus>({
+    url: `${protocol}://${host}/api//blob/latest/total_count_status`,
+  });
+
+  const {
+    cumulativeApplicants, dropouts, totalParticipants, totalProjects,
+  } = totalCountStatus;
+
   return (
-    <ServerErrorBoundary<TotalCountStatus> apiRequest={{
-      url: `${protocol}://${process.env.VERCEL_URL}/blob/latest/total_count_status`,
-    }}
-    >
-      {({
-        cumulativeApplicants, totalParticipants, totalProjects, dropouts,
-      }) => (
-        <>
-          <PageTitle
-            title="지원자 수 카드 섹션"
-            subTitle="캐시 적용으로 실제 적용까지는 최대 5분정도 소요됩니다."
-          />
-          <div className={styles.counterCardWrapper}>
-            <CounterCard count={cumulativeApplicants} title="누적 지원자 수" />
-            <CounterCard count={totalParticipants} title="총 참가자 수" />
-            <CounterCard count={totalProjects} title="총 프로젝트 수" suffix="개" />
-            <CounterCard count={dropouts} title="이탈자 수" color="primary" />
-          </div>
-          <TotalCountStatusForm initialTotalCountStatus={{
-            cumulativeApplicants, totalParticipants, totalProjects, dropouts,
-          }}
-          />
-        </>
-      )}
-    </ServerErrorBoundary>
+    <>
+      <PageTitle
+        title="지원자 수 카드 섹션"
+        subTitle="캐시 적용으로 실제 적용까지는 최대 5분정도 소요됩니다."
+      />
+      <div className={styles.counterCardWrapper}>
+        <CounterCard count={cumulativeApplicants} title="누적 지원자 수" />
+        <CounterCard count={totalParticipants} title="총 참가자 수" />
+        <CounterCard count={totalProjects} title="총 프로젝트 수" suffix="개" />
+        <CounterCard count={dropouts} title="이탈자 수" color="primary" />
+      </div>
+      <TotalCountStatusForm initialTotalCountStatus={totalCountStatus} />
+    </>
   );
 }
 
