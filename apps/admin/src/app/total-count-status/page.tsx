95484d6237bd4bcc78622c85b994f33b94211670
saseungmin <dbd02169@naver.com>
2024-09-17 18:30:47 +0900

    refactor: serverErrorboundary를 hoc로 refactoring 및 ui 패키지로 이동

--- apps/admin/src/app/total-count-status/page.tsx
+++ apps/admin/src/app/total-count-status/page.tsx
@@ -1,41 +1,41 @@
 import { type TotalCountStatus } from '@dnd-academy/core';
 import { CounterCard, PageTitle } from '@dnd-academy/ui';
+import { withServerErrorBoundary } from '@dnd-academy/ui/server';
 
-import ServerErrorBoundary from '@/components/common/ServerErrorBoundary';
 import TotalCountStatusForm from '@/components/TotalCountStatusForm';
 
 import styles from './page.module.scss';
 
-async function page() {
-  const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
+type Props = {
+  data: TotalCountStatus;
+};
+
+async function Page({ data }: Props) {
+  const {
+    cumulativeApplicants, dropouts, totalParticipants, totalProjects,
+  } = data;
 
   return (
-    <ServerErrorBoundary<TotalCountStatus> apiRequest={{
-      url: `${protocol}://${process.env.VERCEL_URL}/blob/latest/total_count_status`,
-    }}
-    >
-      {({
+    <>
+      <PageTitle
+        title="지원자 수 카드 섹션"
+        subTitle="캐시 적용으로 실제 적용까지는 최대 5분정도 소요됩니다."
+      />
+      <div className={styles.counterCardWrapper}>
+        <CounterCard count={cumulativeApplicants} title="누적 지원자 수" />
+        <CounterCard count={totalParticipants} title="총 참가자 수" />
+        <CounterCard count={totalProjects} title="총 프로젝트 수" suffix="개" />
+        <CounterCard count={dropouts} title="이탈자 수" color="primary" />
+      </div>
+      <TotalCountStatusForm initialTotalCountStatus={{
         cumulativeApplicants, totalParticipants, totalProjects, dropouts,
-      }) => (
-        <>
-          <PageTitle
-            title="지원자 수 카드 섹션"
-            subTitle="캐시 적용으로 실제 적용까지는 최대 5분정도 소요됩니다."
-          />
-          <div className={styles.counterCardWrapper}>
-            <CounterCard count={cumulativeApplicants} title="누적 지원자 수" />
-            <CounterCard count={totalParticipants} title="총 참가자 수" />
-            <CounterCard count={totalProjects} title="총 프로젝트 수" suffix="개" />
-            <CounterCard count={dropouts} title="이탈자 수" color="primary" />
-          </div>
-          <TotalCountStatusForm initialTotalCountStatus={{
-            cumulativeApplicants, totalParticipants, totalProjects, dropouts,
-          }}
-          />
-        </>
-      )}
-    </ServerErrorBoundary>
+      }}
+      />
+    </>
   );
 }
 
-export default page;
+export default withServerErrorBoundary(Page, {
+  url: '/blob/latest/total_count_status',
+  type: 'bff',
+});
