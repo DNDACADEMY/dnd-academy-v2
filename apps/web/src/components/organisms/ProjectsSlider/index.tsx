ab4c38a4052980d1a377ec1dc281cb39bfface48
Harang <dbd02169@naver.com>
2024-10-20 21:02:07 +0900

    feat: dnd 11기 프로젝트 thumbnail, pdf 추가 (#207)

--- apps/web/src/components/organisms/ProjectsSlider/index.tsx
+++ apps/web/src/components/organisms/ProjectsSlider/index.tsx
@@ -3,16 +3,10 @@ import Marquee from 'react-fast-marquee';
 import Image from 'next/image';
 import Link from 'next/link';
 
-import { type Project } from '@dnd-academy/core';
-
 import { getProjects } from '@/lib/apis/project';
 import blurDataUrl from '@/lib/data/blurDataUrl';
 
 import styles from './index.module.scss';
-
-type ThumbnailProject = Project & {
-  thumbnail: string;
-};
 
 function ProjectsSlider() {
   const projects = getProjects();
@@ -22,7 +16,6 @@ function ProjectsSlider() {
       <Marquee pauseOnHover speed={100} className={styles.marquee}>
         <div className={styles.projectSliderWrapper}>
           {[...projects]
-            .filter((project): project is ThumbnailProject => Boolean(project?.thumbnail))
             .slice(0, 20)
             .map(({ id, thumbnail, title }) => (
               <Link key={id} href={`/projects/${id}`} prefetch={false}>
