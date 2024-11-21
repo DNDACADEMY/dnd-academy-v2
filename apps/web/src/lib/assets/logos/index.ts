7b3f383028184c882978ce81ecc565376f4f06f1
Harang <dbd02169@naver.com>
2024-11-21 23:54:19 +0900

    refactor: 사용하지 않는 로고 svg 삭제 (#231)

--- apps/web/src/lib/assets/logos/index.ts
+++ apps/web/src/lib/assets/logos/index.ts
@@ -3,7 +3,6 @@ import BehanceLogo from './behance.svg';
 import DesignerBadge from './designer-badge.svg';
 import DeveloperBadge from './developer-badge.svg';
 import DNDLogo from './dnd.svg';
-import EliceLogo from './elice.svg';
 import EmailLogo from './email.svg';
 import FacebookLogo from './facebook.svg';
 import FigmaLogo from './figma.svg';
@@ -14,7 +13,6 @@ import InstagramLogo from './instagram.svg';
 import LinkedInLogo from './linkedin.svg';
 import MediumLogo from './medium.svg';
 import NaverLogo from './naver.svg';
-import NotefolioLogo from './notefolio.svg';
 import TistoryLogo from './tistory.svg';
 import VelogLogo from './velog.svg';
 import YoutubeLogo from './youtube.svg';
@@ -25,7 +23,6 @@ export {
   DesignerBadge,
   DeveloperBadge,
   DNDLogo,
-  EliceLogo,
   EmailLogo,
   FacebookLogo,
   FigmaLogo,
@@ -36,7 +33,6 @@ export {
   LinkedInLogo,
   MediumLogo,
   NaverLogo,
-  NotefolioLogo,
   TistoryLogo,
   VelogLogo,
   YoutubeLogo,
