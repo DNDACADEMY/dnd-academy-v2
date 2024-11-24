951f1ac9ffb9d3e7c8400b5b2b26b3be7eec5652
Harang <dbd02169@naver.com>
2024-11-24 23:45:38 +0900

    feat(web): share alarm section 크리스마스 theme 적용 (#235)

--- apps/web/src/components/organisms/ShareAlarmSection/index.tsx
+++ apps/web/src/components/organisms/ShareAlarmSection/index.tsx
@@ -1,12 +1,15 @@
 import { CSSProperties } from 'react';
 import Marquee from 'react-fast-marquee';
 
+import Image from 'next/image';
+
 import { Button } from '@dnd-academy/ui';
 import clsx from 'clsx';
 
 import ShareClipBoardCTA from '@/components/molecules/ShareClipBoardCTA';
 import ApplyNotifyButtonGroup from '@/components/organisms/ApplyNotifyButtonGroup';
 import { getEventStatus } from '@/lib/apis/event';
+import { isChristmasTheme } from '@/utils';
 
 import styles from './index.module.scss';
 
@@ -33,27 +36,38 @@ const marqueeTexts: MarqueeText[] = [
 const dividerStyles: DividerStyles[] = [{
   id: 1,
   height: '20px',
-  marginBottom: '24px',
 }, {
   id: 2,
-  height: '16px',
-  marginBottom: '16px',
+  height: '24px',
 }, {
   id: 3,
   height: '16px',
-  marginBottom: '8px',
 }, {
   id: 4,
   height: '16px',
-  marginBottom: '4px',
 }, {
   id: 5,
+  height: '16px',
+}, {
+  id: 6,
+  height: '8px',
+}, {
+  id: 7,
+  height: '16px',
+}, {
+  id: 8,
+  height: '4px',
+}, {
+  id: 9,
   height: '20px',
-  marginBottom: '2px',
+}, {
+  id: 10,
+  height: '2px',
 }];
 
 function ShareAlarmSection() {
   const eventStatus = getEventStatus();
+  const isChristmas = isChristmasTheme();
 
   const shareUrl = process.env.NEXT_PUBLIC_ORIGIN;
   const introduceText = 'DND와 함께 프로젝트를 시작해 보세요!\n1. 개발자와 디자이너의 협업 경험을 만들 수 있어요.\n2. 헤매지 않고 8주 커리큘럼에 따라 프로젝트를 완성해요.\n3. DND에서 주최하는 다양한 이벤트, 세미나에 참가할 수 있어요.\n지금 바로 참가하기\n';
@@ -61,19 +75,51 @@ function ShareAlarmSection() {
   return (
     <div className={styles.shareAlarmSection}>
       <Marquee autoFill className={styles.marqueeContainer}>
-        <div className={styles.marqueeTexts}>
+        <div className={clsx(styles.marqueeTexts, { [styles.christmasTheme]: isChristmas })}>
           {marqueeTexts.map(({ id, text }) => (
-            <div key={id} className={clsx(id % 2 === 0 ? styles.marqueeEmoji : styles.marqueeText)}>
+            <div
+              key={id}
+              className={clsx(id % 2 === 0 ? styles.marqueeEmoji : styles.marqueeText)}
+            >
               {text}
             </div>
           ))}
         </div>
       </Marquee>
       {dividerStyles.map(({ id, ...style }) => (
-        <div key={id} className={styles.divider} style={style} />
+        <div
+          key={id}
+          style={style}
+          className={clsx(styles.divider, isChristmas && styles.christmasTheme, {
+            [styles.evenDivider]: id % 2 === 0,
+            [styles.oddDivider]: id % 2 === 1,
+          })}
+        />
       ))}
-      <div className={styles.descriptionWrapper}>
-        <h3 className={styles.title}>지금 함께 프로젝트를 시작해요!</h3>
+      <div className={clsx(styles.descriptionWrapper, { [styles.christmasTheme]: isChristmas })}>
+        <h3 className={clsx(isChristmas ? styles.christmasTitle : styles.title)}>
+          {isChristmas
+            ? (
+              <>
+                <Image
+                  src="/assets/images/christmas/leaf-angle.png"
+                  alt="christmas-title"
+                  width={100}
+                  height={80}
+                  className={styles.christmasLeafAngle}
+                />
+                <span className={styles.title}>지금 함께 프로젝트를 시작해요!</span>
+                <Image
+                  src="/assets/images/christmas/leaf-angle.png"
+                  alt="christmas-title"
+                  width={100}
+                  height={80}
+                  className={styles.christmasLeafAngle}
+                />
+              </>
+            )
+            : '지금 함께 프로젝트를 시작해요!'}
+        </h3>
         <div className={styles.buttonWrapper}>
           <ShareClipBoardCTA shareText={`${introduceText}${shareUrl}`}>
             <Button fullWidth size="xLarge" type="button">친구에게 공유하기</Button>
