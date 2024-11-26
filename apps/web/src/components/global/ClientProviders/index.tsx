897407ce8fc926465f87c1d26f3b7418eed9d3d2
saseungmin <dbd02169@naver.com>
2024-11-26 23:52:36 +0900

    chore: 화면 크기에 따라서 눈 양 수정

--- apps/web/src/components/global/ClientProviders/index.tsx
+++ apps/web/src/components/global/ClientProviders/index.tsx
@@ -5,8 +5,26 @@ import PerfectScrollbar from 'react-perfect-scrollbar';
 import { Snowfall } from 'react-snowfall';
 
 import * as ChannelService from '@channel.io/channel-web-sdk-loader';
+import { useMediaQuery } from 'usehooks-ts';
+
+import BREAK_POINT from '@/lib/constants/breakPoint';
 
 function ClientProviders({ children }: { children: ReactNode }) {
+  const isSmall = useMediaQuery(BREAK_POINT.small);
+  const isXLarge = useMediaQuery(BREAK_POINT.xLarge);
+
+  const getSnowflakeCount = () => {
+    if (isXLarge) {
+      return 100;
+    }
+
+    if (isSmall) {
+      return 60;
+    }
+
+    return 80;
+  };
+
   useEffect(() => {
     ChannelService.loadScript();
     ChannelService.boot({ pluginKey: process.env.NEXT_PUBLIC_CHANNEL_IO_PLUGIN_KEY });
@@ -15,7 +33,7 @@ function ClientProviders({ children }: { children: ReactNode }) {
   return (
     <PerfectScrollbar>
       <Snowfall
-        snowflakeCount={100}
+        snowflakeCount={getSnowflakeCount()}
         style={{
           position: 'fixed',
           width: '100vw',
