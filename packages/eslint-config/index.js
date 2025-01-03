c2c7c39effa8f591482898d577caa74090f5dbba
saseungmin <dbd02169@naver.com>
2025-01-03 22:41:11 +0900

    feat: [WIP] eslint v9 마이그레이션
    - airbnb rule 걷어내기
    - prettier 적용

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,100 +1,125 @@
-module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    browser: true,
-    jest: true,
-  },
-  extends: [
-    'airbnb',
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:react/jsx-runtime',
-  ],
-  plugins: [
-    'simple-import-sort',
-    'unused-imports',
-  ],
-  ignorePatterns: [
-    'node_modules/',
-    '.pnp.cjs',
-    '.pnp.loader.cjs',
-    'public/',
-    '.yarn/',
-    'dist/',
-    'coverage/',
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      extends: [
-        'airbnb-typescript',
-        'plugin:@typescript-eslint/recommended',
-      ],
-      plugins: [
-        '@typescript-eslint',
-      ],
-      rules: {
-        '@typescript-eslint/no-use-before-define': 'off',
-        '@typescript-eslint/naming-convention': 'off',
-        '@typescript-eslint/no-explicit-any': 'warn',
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import reactPlugin from 'eslint-plugin-react';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import unusedImports from "eslint-plugin-unused-imports";
+import globals from "globals";
+import pluginJest from 'eslint-plugin-jest';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+
+export default [
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
       },
-    },
-    {
-      files: ['src/hooks/**/**/*.test.ts?(x)'],
-      rules: {
-        'react-hooks/rules-of-hooks': 'off',
-      },
-    },
-    {
-      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
-      extends: ['plugin:testing-library/react', 'plugin:jest/recommended'],
-      rules: {
-        'react-hooks/rules-of-hooks': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-      },
-      settings: {
-        jest: {
-          globalAliases: {
-            describe: ['context'],
-          },
+      ecmaVersion: 12,
+      sourceType: 'module',
+      parser: tsparser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
         },
       },
     },
-  ],
-  rules: {
-    'react/require-default-props': 'off',
-    'import/order': 'off',
-    'simple-import-sort/imports': ['error', {
-      groups: [
-        ['^\\u0000'],
-        ['^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)'],
-        ['^react'],
-        ['^next'],
-        ['^@?\\w'],
-        ['^(@|lib|components|utils|hooks)(/.*|$)'],
-        ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
-        ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
-        ['^.+\\.svg$'],
-        ['^.+\\.module.s?css$'],
-      ],
-    }],
-    'simple-import-sort/exports': 'error',
-    'unused-imports/no-unused-imports': 'error',
-    'unused-imports/no-unused-vars': ['warn', {
-      vars: 'all',
-      varsIgnorePattern: '^_',
-      args: 'after-used',
-      argsIgnorePattern: '^_',
-    }],
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    plugins: {
+      'react-refresh': reactRefresh,
+      'react-hooks': reactHooks,
+      react: reactPlugin,
+      prettier: prettierPlugin,
+      "simple-import-sort": simpleImportSort,
+      "unused-imports": unusedImports,
+      'jsx-a11y': jsxA11y,
+    },
+    rules: {
+      ...prettierConfig.rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...reactRefresh.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'jsx-a11y/alt-text': 'error',
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'simple-import-sort/imports': ['error', {
+        groups: [
+          ['^\\u0000'],
+          ['^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)'],
+          ['^react'],
+          ['^next'],
+          ['^@?\\w'],
+          ['^(@|lib|components|utils|hooks)(/.*|$)'],
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          ['^.+\\.svg$'],
+          ['^.+\\.module.s?css$'],
+        ],
+      }],
+      'simple-import-sort/exports': 'error',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': ['warn', {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      }],
+    },
   },
-};
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      '@typescript-eslint/no-use-before-define': 'off',
+      '@typescript-eslint/naming-convention': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+  {
+    files: ["src/hooks/**/**/*.test.ts?(x)"],
+    rules: {
+        "react-hooks/rules-of-hooks": "off",
+    },
+  },
+  {
+    files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+    plugins: { jest: pluginJest },
+    settings: {
+      jest: {
+        globalAliases: {
+          describe: ['context'],
+        },
+      },
+    },
+    rules: {
+      ...pluginJest.configs.recommended.rules,
+      'react-hooks/rules-of-hooks': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+  {
+    ignores: [
+      "**/node_modules/",
+      "**/.pnp.cjs",
+      "**/.pnp.loader.cjs",
+      "**/public/",
+      "**/.yarn/",
+      "**/dist/",
+      "**/coverage/",
+    ],
+  },
+];
