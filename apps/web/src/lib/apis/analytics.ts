432b50c28bda852e8b2d2c9d762396f7319e0511
Jihyeong <iolpd42@gmail.com>
2025-06-24 01:03:38 +0900

    fix: analytics 수정 (#304)

--- apps/web/src/lib/apis/analytics.ts
+++ apps/web/src/lib/apis/analytics.ts
@@ -3,7 +3,7 @@ import { sendGAEvent as _sendGA4Event } from '@next/third-parties/google';
 import { GAEvent } from '../constants/gaEvent';
 
 function sendGA4Event(event: GAEvent, params: Record<string, unknown>) {
-  _sendGA4Event(event, params);
+  _sendGA4Event('event', event, params);
 }
 
 export default sendGA4Event;
