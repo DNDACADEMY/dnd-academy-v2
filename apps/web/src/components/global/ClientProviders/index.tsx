b293ef856916aee932136b943992c9beb2585d27
Zero-1016 <iolpd42@gmail.com>
2025-12-06 20:10:58 +0900

    fix(ui): add next devDependencies

--- apps/web/src/components/global/ClientProviders/index.tsx
+++ apps/web/src/components/global/ClientProviders/index.tsx
@@ -2,24 +2,28 @@
 
 import { ReactNode, useEffect } from 'react';
 import PerfectScrollbar from 'react-perfect-scrollbar';
+import { Snowfall } from 'react-snowfall';
 
 import * as ChannelService from '@channel.io/channel-web-sdk-loader';
+import { useMediaQuery } from 'usehooks-ts';
+
+import BREAK_POINT from '@/lib/constants/breakPoint';
 
 function ClientProviders({ children }: { children: ReactNode }) {
-  // const isSmall = useMediaQuery(BREAK_POINT.small);
-  // const isXLarge = useMediaQuery(BREAK_POINT.xLarge);
+  const isSmall = useMediaQuery(BREAK_POINT.small);
+  const isXLarge = useMediaQuery(BREAK_POINT.xLarge);
 
-  // const getSnowflakeCount = () => {
-  //   if (isXLarge) {
-  //     return 100;
-  //   }
+  const getSnowflakeCount = () => {
+    if (isXLarge) {
+      return 100;
+    }
 
-  //   if (isSmall) {
-  //     return 60;
-  //   }
+    if (isSmall) {
+      return 60;
+    }
 
-  //   return 80;
-  // };
+    return 80;
+  };
 
   useEffect(() => {
     ChannelService.loadScript();
@@ -28,7 +32,7 @@ function ClientProviders({ children }: { children: ReactNode }) {
 
   return (
     <PerfectScrollbar>
-      {/* <Snowfall
+      <Snowfall
         snowflakeCount={getSnowflakeCount()}
         style={{
           position: 'fixed',
@@ -36,7 +40,7 @@ function ClientProviders({ children }: { children: ReactNode }) {
           height: '100vh',
           zIndex: 100,
         }}
-      /> */}
+      />
       {children}
     </PerfectScrollbar>
   );
