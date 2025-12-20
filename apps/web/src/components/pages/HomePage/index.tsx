f75a525e7f6fe4e535a309dd0980cb3c7fb42d93
Jihyeong Lee <iolpd42@gmail.com>
2025-12-20 20:34:28 +0900

    feat: 합격자 조회 버튼에 발표 시간 제약 조건 추가 (#335)
    - applicantAcceptanceDateTime prop 추가
    - 합격 발표 시간 이전에는 버튼 비활성화
    - INACTIVE 상태와 시간 조건을 함께 체크하도록 변경

--- apps/web/src/components/pages/HomePage/index.tsx
+++ apps/web/src/components/pages/HomePage/index.tsx
@@ -11,6 +11,7 @@ import ApplyNotifyButtonGroup from '@/components/organisms/ApplyNotifyButtonGrou
 import CounterCardSection from '@/components/organisms/CounterCardSection';
 import EventStatus from '@/components/organisms/EventStatus';
 import FAQSection from '@/components/organisms/FAQSection';
+import PassboardButton from '@/components/organisms/PassboardButton';
 import ProjectsSlider from '@/components/organisms/ProjectsSlider';
 import ShareAlarmSection from '@/components/organisms/ShareAlarmSection';
 import SponsorSection from '@/components/organisms/SponsorSection';
@@ -67,7 +68,7 @@ function HomePage({
             <ApplyNotifyButtonGroup eventStatus={eventStatus}>
               <Button size="xLarge" buttonType="primary" />
             </ApplyNotifyButtonGroup>
-            {/* <PassboardButton status={eventStatus.status} /> */}
+            <PassboardButton eventStatus={eventStatus} />
           </div>
         </div>
         <div className={styles.bannerWrapper}>
